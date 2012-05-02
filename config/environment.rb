d20b8db694f8c4090a0e1a877b3bb27edc691506
Junior <junior@junior-U-100.(none)>
2012-05-02 12:19:31 -0700

    smtp setup

--- config/environment.rb
+++ config/environment.rb
@@ -6,4 +6,10 @@ GeneralThingsV2::Application.initialize!
 
 ActionMailer::Base.delivery_method = :smtp
 
-
+ActionMailer::Base.smtp_settings = {
+  :address  => "smtp.gmail.com",
+  :port  => 25,
+  :user_name  => "testemailjunior@gmail.com",
+  :password  => "juniorjoanis",
+  :authentication  => :login
+}
