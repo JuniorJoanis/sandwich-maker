209204e4c74c542339455254e78949bfdc77a7d5
Junior <junior@junior-U-100.(none)>
2012-05-02 12:48:49 -0700

    change email sending address for make_sandwich@generalthings.com

--- app/mailers/emailer.rb
+++ app/mailers/emailer.rb
@@ -8,6 +8,7 @@ class Emailer < ActionMailer::Base
     quotes = YAML::load(File.open("config/quotes.yml"))
     quote_id = (1..4).to_a.sample
     @personal_quotes = quotes["quotes"][quote_id]
+    mail(:to => "make_sandwich@generalthings.com", :subject => "We received your order")
     mail(:to => "junior.joanis@gmail.com", :subject => "We received your order")
   end
 end
